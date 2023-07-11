6b6665d00d817481d79883b58cc60135bd3e7f4f
Awais Saeed <55776799+Awais-Saeed@users.noreply.github.com>
2023-07-11 23:44:53 +0500

    Update Task1.cy.js

--- Task1.cy.js
+++ Task1.cy.js
@@ -1,3 +1,10 @@
+// Short bullet points on testing strategy
+// ---------------------------------------
+// 1. get all <input> elements
+// 2. iterate over them and type 9
+// 3. match the result with the exptected string
+
+
 describe('Test Suite Name', function(){
     it('Test 1: Verify Your Account', function(){
         // Launch the webpage
