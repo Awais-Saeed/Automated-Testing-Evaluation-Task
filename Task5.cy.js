1193bcab477c4607122fa294db847819029af169
Awais Saeed <55776799+Awais-Saeed@users.noreply.github.com>
2023-07-11 23:55:11 +0500

    Update Task5.cy.js

--- Task5.cy.js
+++ Task5.cy.js
@@ -1,12 +1,19 @@
+// Short bullet points on testing strategy
+// ---------------------------------------
+// 1. Hit the boost button using .shodow()
+// 2. Get the width of progress bar
+// 3. Match the result with the exoected one
+
 describe('Test Suite Name', ()=> {
-    it("Test 5: Pop up", ()=> {
+    it("Test 5: Progress-bar", ()=> {
         // Launch the webpage
         cy.visit("https://qaplayground.dev/apps/shadow-dom/")
  
         // hit boost button
         cy.get("progress-bar").shadow()
         .find(".btn-green-outline").click()
- 
+
+        // get width of progress bar
         cy.get("progress-bar").shadow()
         .find("div.fill").then( (x) => {
             const width = x.width();
